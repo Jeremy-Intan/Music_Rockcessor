9b596ad012aad55dd4e9cfe3bab6e1b2beba0ce2
ejkburt <47953253+ejkburt@users.noreply.github.com>
2019-11-21 16:53:14 -0600

    Update controller.v

--- verilog/cpu/controller.v
+++ verilog/cpu/controller.v
@@ -1,9 +1,9 @@
-module controller (OpCode, RegWrite, BitmapWrite, DMemWrite, DMemEn, SignEx, MatchAcc, CompAcc, ALUAdd, ALUSub, MuxReadBM, MuxReadReg1, MuxReadReg2, MuxWriteReg, MuxWriteData, MuxPCIn, MuxALUIn);
+module controller (OpCode, RegWrite, BitmapWrite, DMemWrite, DMemEn, SignEx, MatchAcc, CompAcc, ALUAdd, ALUSub, ALUBR, ALULdSt, ALUShift, ALUScale, MuxReadBM, MuxReadReg1, MuxReadReg2, MuxWriteReg, MuxWriteData, MuxPCIn, MuxALUIn);
 
 
 input [3:0] 	OpCode;
 
-output 			RegWrite, BitmapWrite, DMemWrite, DMemEn, MatchAcc, CompAcc, ALUAdd, ALUSub, MuxReadReg2, MuxWriteReg, MuxWriteData;
+output 			RegWrite, BitmapWrite, DMemWrite, DMemEn, MatchAcc, CompAcc, ALUAdd, ALUSub, ALUBR, ALULdSt, ALUShift, ALUScale, MuxReadReg2, MuxWriteReg, MuxWriteData;
 output [1:0]	SignEx, MuxReadBM, MuxReadReg1, MuxPCIn;
 
 assign RegWrite = ((OpCode == 4'b0010)|(OpCode == 4'0011)) ? 1 : 0;
@@ -17,7 +17,10 @@ assign CompAcc = ((OpCode == 4'b1011) | (OpCode == 4'b1010)) ? 1 : 0;
 
 assign ALUAdd = (OpCode == 4'b0011);
 assign ALUSub = (OpCode == 4'b0010);
-
+assign ALUBR = ((OpCode == 4'b0100) | (OpCode == 4'b0101));
+assign ALULdSt = ((OpCode == 4'b0110) | (OpCode == 4'b0111)| (OpCode == 4'b1111) | (OpCode == 4'b1110));
+assign ALUShift = (OpCode == 4'b1011);
+assign ALUScale = (OpCode == 4'b1010);
 
 assign MuxReadBM = (OpCode == 4'b1000) ? 2'b00 : ((OpCode == 4'b1010) | (OpCode == 4'b1011)) ? 2'b01 : 2'b11;
 assign MuxReadReg2 = (OpCode == 4'b1000);
