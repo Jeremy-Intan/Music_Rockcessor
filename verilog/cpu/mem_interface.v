067d9f32f31c632dfdd2c3c3fac0b952f7021c04
ShenghaoX <xueshenghao@gmail.com>
2019-12-05 16:56:29 -0600

    Update mem_interface.v

--- verilog/cpu/mem_interface.v
+++ verilog/cpu/mem_interface.v
@@ -2,16 +2,16 @@
 //wr_enable enables the write, read is always enabled
 //reset is asynch active low
 //all read and writes are assumed to be 1 cycle latency, so no ready bit needed
-module mem_interface(write_addr, read_addr, wr_enable, wr_data, rd_data, clk, rst_n);
+module mem_interface(wraddress, rdaddress, wren, data, q, clock, aclr);
 
 parameter width=16; //the bit width
 
-input wire [15:0] wr_addr;
-input wire [15:0] rd_addr;
-input wire wr_enable;
-input wire [width-1:0] wr_data;
-input clk;
-input rst_n;
-output reg [width-1:0] rd_data;
+input wire [15:0] wraddress;
+input wire [15:0] rdaddress;
+input wire wren;
+input wire [width-1:0] data;
+input clock;
+input aclr;
+output reg [width-1:0] q;
 
 endmodule
